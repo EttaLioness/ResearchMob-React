3749c0c60a64d15e26ac8d403795acf6fb9141cc
Etta <etta.lioness@hotmail.com>
2023-04-08 21:43:55 +1000

    removed owner on project card, added flex wrap

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -13,27 +13,14 @@ import './projectCard.css'
 
 function ProjectCard(props){
     const { projectData } = props;
-    // const { userList } = props;
-    const [userList,setUserList] = useState([])
+    const { userList } = props;
+  
 
-    
-
-    useEffect(() => {
-        fetch(`${import.meta.env.VITE_API_URL}users`) //querying info from api listed in .env  and added the endpoint (project)
-        .then((results) => {
-            return results.json(); //turn it into object jason
-        })
-        .then((data) => {
-            setUserList(data)
-        });
-        // setProjectList(allProjects)
-    }, []);
-
-    const owner = userList.find((user)=>user.id==projectData.owner)
-    console.log(owner)
+    // const owner = userList.find((user)=>user.id==projectData.owner);
+    // console.log(owner)
 
     const percentageGoalReached = (projectData.total_amount_pledged / projectData.goal)*100
-    const htmlpercentageGoalReached = percentageGoalReached.toFixed(2)
+    const htmlpercentageGoalReached = percentageGoalReached?.toFixed(2)
     
 
     return (
@@ -41,7 +28,7 @@ function ProjectCard(props){
                 <Link to={`/project/${projectData.id}`}>
                     <img src={projectData.image}></img>
                     <h2 className="title">{projectData.title}</h2>
-                    <section className="userDetails">
+                    {/* <section className="userDetails">
                         <div className="imgContainer">
                             <img src={owner?.image} alt="" /> 
                         </div>
@@ -49,7 +36,7 @@ function ProjectCard(props){
                             <h6>{owner?.first_name}{owner?.last_name}</h6>
                             <p>{owner?.affiliate}</p>
                         </div>
-                    </section>
+                    </section> */}
                     
                     <div className="moneyValues">
                         <span>{htmlpercentageGoalReached}%</span>
